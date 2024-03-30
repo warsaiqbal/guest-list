dec6a9a07bb1f4a8741c3645adb4cec6184c6d04
warsaiqbal <160345806+warsaiqbal@users.noreply.github.com>
2024-03-30 08:42:51 -0700

    Add files via upload

---
+++ 13. Guest list/app.js
@@ -0,0 +1,4 @@
+var guests = ["Alishba", "Faizan", "Adan"];
+console.log("I'd like to invite", guests[0], "to dinner.");
+console.log("I'd like to invite", guests[1], "to dinner.");
+console.log("I'd like to invite", guests[2], "to dinner.");
